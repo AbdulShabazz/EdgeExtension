6c141569f273e2ba3aaacd10710358ae0c420ce0
AbdulShabazz <AbdulShabazz@user.github.com>
2025-03-16 12:08:30 -0400

    modified:   extract_english_translation_content_script.js (Fix: connection timeout error by embedding setTimeout into function call)

--- extract_english_translation_content_script.js
+++ extract_english_translation_content_script.js
@@ -32,13 +32,27 @@ function connectPort() {
     port.onMessage.addListener((message, sender, sendResponse) => {
         switch (message.action) {
         case 'generateEnglishPrompt':
-            const inputText = document.querySelector('textarea');
-            const iid = setTimeout (() => {
-                clearTimeout (iid);
-                const stranslatedText = document.querySelector('[jsname="W297wb"]');
-                const langElement = document.querySelector('[class="VfPpkd-jY41G-V67aGc"]').textContent.replace(/\s*\-\s*Detected$/,'');
-                message.prompt = `${translatedText.textContent} (Original ${langElement} Prompt: ${message.prompt})`;
-            }, 800);
+            function buildPrompt () {
+                const inputText = document.querySelector('textarea');
+                const translatedText = document.querySelector('[jsname="W297wb"]');
+                if (!translatedText)
+                    return false;
+                if (translatedText != inputText){
+                    const langElement = document.querySelector('[class="VfPpkd-jY41G-V67aGc"]').textContent.replace(/\s*\-\s*Detected$/,'');
+                    message.prompt = `${translatedText.textContent} (Original ${langElement} Prompt: ${message.prompt})`;
+                }
+                return true;
+            } // end buildPrompt
+            function siteReady (){
+                let iid = setTimeout (() => {
+                    if (buildPrompt ())
+                        clearTimeout (iid);
+                    else {
+                        siteReady ();
+                    };
+                }, 500);
+            } // end siteReady
+            siteReady ();
             message.action = "EnglishPromptCompleted";
             port.postMessage (message);
             break;
