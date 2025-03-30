088a947192eae270d1db8a200af977ce0cef2c78
AbdulShabazz <AbdulShabazz@user.github.com>
2025-03-30 10:28:29 -0400

    modified:   background.js (Strip html markup from prompts)

--- background.js
+++ background.js
@@ -120,6 +120,7 @@ function stripSymbols (promptW) {
     const prompt = promptW
       .replace(/[\u{1F300}-\u{1F6FF}\u{1F900}-\u{1FAFF}\u2700-\u27BF\u2600-\u26FF\u2190-\u21FF\u2500-\u257F\u2B50-\u2BFF\uFE0F]/gu, '')
       .replace(/\s+/g, ' ')
+      .replace (/\W+/g, ' ')
       .substring (0, MAXLENGTH);
     if (promptW.length < MAXLENGTH)
         return prompt;
