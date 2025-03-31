71bdf1ef2e259bce860f0d925bfe3d831cd0d29c
AbdulShabazz <AbdulShabazz@user.github.com>
2025-03-31 00:15:49 -0400

    modified:   background.js (Undo changes; add special cases)

--- background.js
+++ background.js
@@ -118,9 +118,8 @@ function truncateToQuota(inputString, quota) {
 function stripSymbols (promptW) {
     const MAXLENGTH = 2230;
     const prompt = promptW
-      .replace(/[\u{1F300}-\u{1F6FF}\u{1F900}-\u{1FAFF}\u2700-\u27BF\u2600-\u26FF\u2190-\u21FF\u2500-\u257F\u2B50-\u2BFF\uFE0F]/gu, '')
+      .replace(/[\u{1F300}-\u{1F6FF}\u{1F900}-\u{1FAFF}\u2700-\u27BF\u2600-\u26FF\u2190-\u21FF\u2500-\u257F\u2B50-\u2BFF\uFE0F\*<>]/gu, '')
       .replace(/\s+/g, ' ')
-      .replace (/\W+/g, ' ')
       .substring (0, MAXLENGTH);
     if (promptW.length < MAXLENGTH)
         return prompt;
